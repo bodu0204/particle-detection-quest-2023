f1b4609e4fd25ec806f97774d6e218ae9761c492
tmyuu <tmyuu@proton.me>
2023-11-17 00:46:28 +0900

    add plot_confusion_matrix_and_accuracy

--- analysis/test.py
+++ analysis/test.py
@@ -10,12 +10,24 @@ def normalize_map(map, resize_shape=(28, 28)):
 
     # 0の値のインデックスを取得
     y_indices, x_indices = np.where(map == 0)
-
     # 0の値を置換
     map[y_indices, x_indices] = map[(y_indices + y_add) % len_y, (x_indices + x_add) % len_x]
-
     # リサイズし、1を減算
     return np.asarray(Image.fromarray(map - 1.0).resize(resize_shape))
+
+def preprocess_map(train_df, normalize_map_func):
+    # データの正規化
+    normalized_train_maps = np.array([normalize_map_func(x) for x in train_df['waferMap']])
+
+    # データ拡張（90度回転、水平反転など）
+    normalized_train_maps = np.concatenate((normalized_train_maps, np.rot90(normalized_train_maps, k=2, axes=(1, 2))), axis=0)
+    normalized_train_maps = np.concatenate((normalized_train_maps, np.rot90(normalized_train_maps, k=1, axes=(1, 2))), axis=0)
+    normalized_train_maps = np.concatenate((normalized_train_maps, np.swapaxes(normalized_train_maps, 1, 2)), axis=0)
+
+    # データの形状を変更
+    normalized_train_maps = normalized_train_maps.reshape(normalized_train_maps.shape + (1,))
+
+    return normalized_train_maps
     
 def create_model(input_shape=(28, 28, 3), num_classes=1000):
     import tensorflow as tf
@@ -32,40 +44,67 @@ def create_model(input_shape=(28, 28, 3), num_classes=1000):
         layers.Dense(8),
     ])
 
-def solution(x_test_df, train_df):
-    import tensorflow as tf
+def calculate_class_weights(train_labels):
     from sklearn.utils.class_weight import compute_class_weight
-
-    resize_shape = (28,28)
-    failure_types = list(train_df['failureType'].unique())
-
-    # 前処理
-    normalized_train_maps = np.array([normalize_map(x) for x in train_df['waferMap']])
-    print(normalized_train_maps.shape)
-    normalized_train_maps = np.concatenate((normalized_train_maps, np.rot90(normalized_train_maps, k=2, axes=(1, 2))), axis=0)
-    normalized_train_maps = np.concatenate((normalized_train_maps, np.rot90(normalized_train_maps, k=1, axes=(1, 2))), axis=0)
-    normalized_train_maps = np.concatenate((normalized_train_maps, np.swapaxes(normalized_train_maps, 1, 2)), axis=0)
-    normalized_train_maps = normalized_train_maps.reshape(normalized_train_maps.shape + (1,))
-    train_labels = np.array([failure_types.index(x) for x in train_df['failureType']] * 8)
-    print(normalized_train_maps.shape)
-    print(train_labels.shape)
     # クラスの重みを計算
     class_weights = compute_class_weight(class_weight='balanced', 
                                          classes=np.unique(train_labels), 
                                          y=train_labels)
     # クラスの重みを辞書型に変換
-    class_weights = dict(enumerate(class_weights))
+    return dict(enumerate(class_weights))
+
+def plot_confusion_matrix_and_accuracy(y_true, y_pred, classes):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    from sklearn.metrics import confusion_matrix
+    """
+    Confusion matrixをプロットし、各クラスの正確さを計算して表示します。
+
+    :param y_true: 実際のラベルのDataFrame
+    :param y_pred: 予測されたラベルのDataFrame
+    :param classes: クラスのリスト
+    """
+    # Confusion matrixの計算
+    cm = confusion_matrix(y_true, y_pred, labels=classes)
+
+    # ヒートマップとしてプロット
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
+    plt.title('Confusion Matrix')
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show()
+
+    # 各クラスごとの正確さを計算して表示
+    print("\nClass Accuracy:")
+    for i, class_name in enumerate(classes):
+        accuracy = cm[i, i] / cm[i, :].sum()
+        print(f"{class_name}: {accuracy * 100:.2f}%")
+
+def solution(x_test_df, train_df):
+    import tensorflow as tf
+
+    resize_shape = (28,28)
+    failure_types = list(train_df['failureType'].unique())
+
+    # 前処理
+    normalized_train_maps = preprocess_map(train_df, normalize_map)
+    train_labels = np.array([failure_types.index(x) for x in train_df['failureType']] * 8)
+
+    print(normalized_train_maps.shape)
+    print(train_labels.shape)
+    class_weights = calculate_class_weights(train_labels)
+
     model = create_model(input_shape=normalized_train_maps[0].shape,num_classes=len(failure_types))
     model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
     model.fit(normalized_train_maps, train_labels, epochs=2, class_weight=class_weights)
-
-    
     normalized_test_maps = np.array([normalize_map(x) for x in x_test_df['waferMap']])
     normalized_test_maps = normalized_test_maps.reshape(normalized_test_maps.shape + (1,))
+
     test_index = [x for x in x_test_df['waferIndex']]
-    model.predict(normalized_test_maps)
+
     predictions = tf.nn.softmax(model.predict(normalized_test_maps)).numpy()
     answer = [failure_types[x.argmax()] for x in predictions]
     return pd.DataFrame({'failureType': answer}, index=x_test_df.index)
@@ -81,6 +120,7 @@ x_test_df = test_df.drop(columns=['failureType'])
 
 # solution関数を実行
 user_result_df = solution(x_test_df, train_df)
+plot_confusion_matrix_and_accuracy(y_test_df['failureType'], user_result_df['failureType'], df['failureType'].unique())
 
 average_accuracy = 0
 # ユーザーの提出物のフォーマット確認
